de8b0824cb6b80793b44edb9aa7213f341ef9042
Karen (Lianhui) Qin <lianhuiqin9@gmail.com>
2019-03-04 22:50:57 -0800

    Update run_docker.sh

--- model/run_docker.sh
+++ model/run_docker.sh
@@ -1,10 +1,5 @@
 CRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-IMAGE=allenlao/pytorch-allennlp-rtd # docker image
-#IMAGE=allenlao/pytorch-allennlp-rt # docker image
-#IMAGE=allenlao/pytorch-allennlp-v2 # docker image
-#IMAGE=allenlao/pytorch-allennlp # docker image
-#IMAGE=allenlao/pytorchv4 # docker image
-
+IMAGE=allenlao/pytorch-allennlp-rt # docker image
 
 echo $SCRIPTPATH
 
@@ -12,7 +7,5 @@ echo $SCRIPTPATH
 docker run \
 -it --rm \
 --net host \
---volume $CRIPTPATH:/san_mrc \
+--volume $CRIPTPATH:/san \
 --interactive --tty $IMAGE /bin/bash
-# --volume /home/data/:/cs \
-# --runtime=nvidia \
